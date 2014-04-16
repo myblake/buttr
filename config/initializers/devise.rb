145e1cf67746eb1f0087e916322a699a0adc0922
Matt Blake <myblake@gmail.com>
2014-04-16 14:57:03 -0700

    mailer name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'robots@myeasyfridge.com'
+  config.mailer_sender = '"EasyFridge" <robots@myeasyfridge.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
