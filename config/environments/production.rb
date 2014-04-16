9338bf69421d273cf91e3907045a2722d85943b8
Matt Blake <myblake@gmail.com>
2014-04-16 14:55:41 -0700

    production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Buttr::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'myeasyfridge.com' }
+  config.action_mailer.default_url_options = { :host => 'app.myeasyfridge.com' }
 end
